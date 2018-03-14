8714dd7f3f15232138cedd151908ddd8727f9f97
lthornton1 <lthornton1@mail.csuchico.edu>
2018-03-13 22:23:04 -0700

    with vectors test one

--- main.cpp
+++ main.cpp
@@ -1,21 +1,46 @@
 #include<iostream> //Author: LogenTHornton
 #include<string>
+#include<vector>
+
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector<string> list; 
   int numItems = 0;
   char input;
-  
+
+  do {
     cout<<"\n==GROCERY LIST MANAGER==";
     cout<<"\nEnter your choice: ";
- do{
-   cout<<"\n (A)dd an item";
+
+
+    cout<<"\n (A)dd an item";
     cout<<"\n (Q)uit";
     cout<<"\nYour choice (A/Q): ";
     cin>>input;
+
+
+
+    if ((input == 'a' || input == 'A')){
+      cout << "What is the item?"<<endl;
+      string filler;
+
+      cin >> filler;
+
+      list.push_back(filler);
+
+      numItems++;
+    }
   } while (input != 'q' && input != 'Q');
+
+  cout << "==ITEMS TO BUY==" <<endl;
+  for (int i=0; i<list.size(); i++){
+    cout << i+1 <<" "<< list[i]<<endl;
+  }
+  if (list.size() == 0){
+    cout << "No items to buy!" << endl;
+  }
 
   return 0;
 }
